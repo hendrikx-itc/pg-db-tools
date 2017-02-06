efc884cff5b133692f2adb549325a417d2e2238b
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2017-02-06 09:17:26 +0100

    Introduce types for schema model

---
+++ src/pg_db_tools/types.py
@@ -0,0 +1,114 @@
+import yaml
+
+
+class PgDatabase:
+    def __init__(self):
+        self.name = None
+        self.extensions = []
+        self.schemas = {}
+
+    def register_schema(self, name):
+        if name in self.schemas:
+            return self.schemas.get(name)
+        else:
+            schema = PgSchema(name)
+
+            self.schemas[name] = schema
+
+            return schema
+
+
+def load(infile):
+    data = yaml.load(infile)
+
+    database = PgDatabase()
+
+    database.extensions = data.get('extensions', [])
+
+    types = [load_type(database, type_data) for type_data in data['types']]
+
+    objects = [load_object(database, object_data) for object_data in data['objects']]
+
+    tables = [object for object in objects if type(object) is PgTable]
+
+    return database
+
+
+def load_type(database, type_data):
+    type_type, object_data = next(iter(type_data.items()))
+
+    if type_type == 'enum':
+        return PgEnum.load(database, object_data)
+    else:
+        raise Exception('Unsupported type: {}'.format(type_type))
+
+
+def load_object(database, object_data):
+    object_type, object_data = next(iter(object_data.items()))
+
+    if object_type == 'table':
+        return PgTable.load(database, object_data)
+    else:
+        raise Exception('Unsupported object type: {}'.format(object_type))
+
+
+class PgSchema:
+    def __init__(self, name):
+        self.name = name
+        self.types = []
+        self.tables = []
+
+
+class PgTable:
+    def __init__(self, schema, name, columns):
+        self.schema = schema
+        self.name = name
+        self.columns = columns
+
+    @staticmethod
+    def load(database, data):
+        schema = database.register_schema(data['schema'])
+
+        table = PgTable(
+            schema,
+            data['name'],
+            [
+                PgColumn.load(database, column_data)
+                for column_data in data['columns']
+            ]
+        )
+
+        schema.tables.append(table)
+
+        return table
+
+
+class PgColumn:
+    def __init__(self, name, data_type):
+        self.name = name
+        self.data_type = data_type
+        self.nullable = False
+
+    @staticmethod
+    def load(database, data):
+        column = PgColumn(
+            data['name'],
+            data['data_type']
+        )
+
+        return column
+
+
+class PgEnum:
+    def __init__(self, schema, name, values):
+        self.schema = schema
+        self.name = name
+        self.values = values
+
+    @staticmethod
+    def load(database, data):
+        return PgEnum(
+            database.register_schema(data['schema']),
+            data['name'],
+            data['values']
+        )
---
+++ tests/test_load.py
@@ -0,0 +1,69 @@
+"""
+Test the load function for schema data from a YAML file.
+"""
+import unittest
+from io import StringIO
+
+from pg_db_tools.types import load
+
+
+json_data = """
+extensions:
+  - btree_gist
+
+types:
+  - enum:
+      name: order_state
+      schema: shop
+      values:
+        - new
+        - shipped
+        - cancelled
+
+objects:
+  - table:
+      name: Order
+      schema: shop
+      description: "Contains all orders"
+      columns:
+        - name: id
+          description: "Primary key"
+          data_type: integer
+        - name: created
+          data_type: timestamp with time zone
+      primary_key: ['id']
+
+  - table:
+      name: OrderLine
+      schema: shop
+      description: "Contains all order lines for all orders"
+      columns:
+        - name: id
+          description: "Primary key"
+          data_type: integer
+        - name: order_id
+          data_type: integer
+        - name: line_nr
+          data_type: integer
+        - name: product_id
+          data_type: integer
+        - name: amount
+          data_type: integer
+      foreign_keys:
+        - columns:
+            - order_id
+          references:
+            table: Order
+            columns:
+              - id
+"""
+
+
+class TestLoad(unittest.TestCase):
+
+    def test_load(self):
+        database = load(StringIO(json_data))
+
+        self.assertEqual(len(database.schemas), 1)
+
+        self.assertTrue('shop' in database.schemas)
