80c83e3b8867101f1391fdf81a3c9027e1583516
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2017-02-06 14:31:56 +0100

    Make foreign key edge rendering configurable

--- src/pg_db_tools/dot_renderer.py
+++ src/pg_db_tools/dot_renderer.py
@@ -1,68 +1,101 @@
 
 
+FOREIGN_KEY_EDGE_CONNECT_PORT = 1
+FOREIGN_KEY_EDGE_CONNECT_NODE = 2
+
+
 class DotRenderer:
-    @staticmethod
-    def render(out_file, database):
-        rendered_chunks = render_dot_chunks(database)
+    def __init__(self):
+        self.label_foreign_key_edges = True
+        self.foreign_key_edge_mode = FOREIGN_KEY_EDGE_CONNECT_NODE
+
+    def render(self, out_file, database):
+        rendered_chunks = self.render_dot_chunks(database)
 
         out_file.writelines(rendered_chunks)
 
+    def render_dot_chunks(self, database):
+        yield 'digraph schema {\n'
 
-def render_dot_chunks(database):
-    yield 'digraph schema {\n'
+        for schema_name, schema in database.schemas.items():
+            yield '  subgraph cluster_{} {{\n'.format(schema_name)
+            yield '    label = "{}"'.format(schema_name)
 
-    for schema_name, schema in database.schemas.items():
-        yield '  subgraph cluster_{} {{\n'.format(schema_name)
-        yield '    label = "{}"'.format(schema_name)
+            for table in schema.tables:
+                yield self.render_table_node(table)
+                yield self.render_table_edges(table)
 
-        for table in schema.tables:
-            yield render_table_node(table)
-            yield render_table_edges(table)
+            yield '}\n'
 
         yield '}\n'
 
-    yield '}\n'
+    def render_table_node(self, table):
+        return (
+            '{} [\n'
+            '  shape = none\n'
+            '  label = {}\n'
+            ']\n'
+        ).format(table_node_name(table.schema.name, table.name), self.render_table_html_label(table))
+
+    def render_table_edges(self, table):
+        return ''.join(
+            self.render_foreign_key(table, foreign_key)
+            for foreign_key in table.foreign_keys
+        )
+
+    def render_foreign_key(self, table, foreign_key):
+        attributes = {}
+
+        if self.label_foreign_key_edges:
+            attributes['label'] = '{port} = {dest_port}'.format(
+                port=foreign_key['columns'][0],
+                dest_port=foreign_key['references']['columns'][0]
+            )
+
+        if self.foreign_key_edge_mode == FOREIGN_KEY_EDGE_CONNECT_PORT:
+            source = '{node_name}:{port}'.format(
+                node_name=table_node_name(table.schema.name, table.name),
+                port=foreign_key['columns'][0]
+            )
+            target = '{dest_node_name}:{dest_port}'.format(
+                dest_node_name=table_node_name(
+                    foreign_key['references']['table']['schema'],
+                    foreign_key['references']['table']['name']
+                ),
+                dest_port=foreign_key['references']['columns'][0]
+            )
+        else:
+            source = '{node_name}'.format(
+                node_name=table_node_name(table.schema.name, table.name)
+            )
+            target = '{dest_node_name}'.format(
+                dest_node_name=table_node_name(
+                    foreign_key['references']['table']['schema'],
+                    foreign_key['references']['table']['name']
+                )
+            )
+
+        return '{source} -> {target} [ {attributes} ];\n'.format(
+            source=source,
+            target=target,
+            attributes=' '.join('{}="{}"'.format(key, value) for key, value in attributes.items())
+        )
+
+    def render_table_html_label(self, table):
+        return (
+            '<<table border="1" cellspacing="0" cellborder="0">\n'
+            '  <tr><td bgcolor="grey" colspan="2">{name}</td></tr>\n'
+            '{column_rows}\n'
+            '</table>>\n'
+        ).format(
+            name=table.name,
+            column_rows='\n'.join(
+                '  <tr><td port="{col_name}" align="left">{col_name}</td>'
+                '<td align="left">{data_type}</td></tr>'.format(col_name=c.name, data_type=c.data_type)
+                for c in table.columns
+            )
+        )
 
 
 def table_node_name(schema_name, table_name):
     return '{}_{}'.format(schema_name, table_name)
-
-
-def render_table_node(table):
-    return (
-        '{} [\n'
-        '  shape = none\n'
-        '  label = {}\n'
-        ']\n'
-    ).format(table_node_name(table.schema.name, table.name), render_table_html_label(table))
-
-
-def render_table_edges(table):
-    return ''.join(
-        '{node_name}:{port} -> {dest_node_name}:{dest_port}\n'.format(
-            node_name=table_node_name(table.schema.name, table.name),
-            port=foreign_key['columns'][0],
-            dest_node_name=table_node_name(
-                foreign_key['references']['table']['schema'],
-                foreign_key['references']['table']['name']
-            ),
-            dest_port=foreign_key['references']['columns'][0]
-        )
-        for foreign_key in table.foreign_keys
-    )
-
-
-def render_table_html_label(table):
-    return (
-        '<<table border="1" cellspacing="0" cellborder="0">\n'
-        '  <tr><td bgcolor="grey" colspan="2">{name}</td></tr>\n'
-        '{column_rows}\n'
-        '</table>>\n'
-    ).format(
-        name=table.name,
-        column_rows='\n'.join(
-            '  <tr><td port="{col_name}" align="left">{col_name}</td>'
-            '<td align="left">{data_type}</td></tr>'.format(col_name=c.name, data_type=c.data_type)
-            for c in table.columns
-        )
-    )
