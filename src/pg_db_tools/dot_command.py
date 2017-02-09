e8eb7e2f5562299a3e244a4e188f2e93eb25b19d
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2017-02-09 16:22:14 +0100

    Also use utf-8 as default encoding for dot and sql sub-commands

--- src/pg_db_tools/dot_command.py
+++ src/pg_db_tools/dot_command.py
@@ -9,7 +9,7 @@ from pg_db_tools.dot_renderer import DotRenderer
 def setup_command_parser(subparsers):
     parser_dot = subparsers.add_parser('dot', help='command for generating Graphviz DOT')
 
-    parser_dot.add_argument('infile', type=argparse.FileType('r'))
+    parser_dot.add_argument('infile', type=argparse.FileType('r', encoding='utf-8')))
     parser_dot.add_argument('--output-file', '-o', help='write output to file', default=sys.stdout)
 
     parser_dot.set_defaults(cmd=dot_command)
