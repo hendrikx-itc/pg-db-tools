aedfc7312a4ef10ae169c967aaa6d6da6b9cae34
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2018-04-11 16:22:52 +0200

    Update extract command description

--- src/pg_db_tools/commands/extract.py
+++ src/pg_db_tools/commands/extract.py
@@ -3,7 +3,7 @@ from pg_db_tools.commands import extract_from_db
 
 def setup_command_parser(subparsers):
     cmd = subparsers.add_parser(
-        'extract', help='compile output from schema definition'
+        'extract', help='extract schema definition from a source'
     )
 
     cmd_subparsers = cmd.add_subparsers()
